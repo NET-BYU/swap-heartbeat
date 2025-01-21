5c46935ea9516b2fb0edb88634e6ef6fb64c9614
RF Enclosure <chkitras@gmail.com>
2025-01-21 11:12:42 -0700

    static volatile instead of extern

--- swap_heartbeat/packet_sender.c
+++ swap_heartbeat/packet_sender.c
@@ -13,7 +13,7 @@
 #include <time.h>
 #include <unistd.h>
 
-extern int *stop_flag; // Stop flag
+static volatile int stop_flag = 0; // Stop flag
 
 // Function prototypes
 int start_packet_transmission(const char *iface, const char *src_mac,
@@ -97,8 +97,6 @@ int start_packet_transmission(const char *iface, const char *src_mac,
   clock_gettime(CLOCK_MONOTONIC, &ts);
   long long start_time = ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
   
-  *stop_flag = 0;
-
   while (!stop_flag) {
     memset(packet, 0, sizeof(packet));
 
@@ -124,5 +122,5 @@ int start_packet_transmission(const char *iface, const char *src_mac,
 
 // Function to handle the stop signal from Python
 void stop_transmission() {
-    *stop_flag = 1;
+    stop_flag = 1;
 }
